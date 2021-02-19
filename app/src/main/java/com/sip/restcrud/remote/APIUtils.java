ca14051666a9bc262b5c3d1980a4ab97358c372f
amine <amine.mezghich@ensi-uma.tn>
2021-02-19 14:39:54 +0100

    last version

--- app/src/main/java/com/sip/restcrud/remote/APIUtils.java
+++ app/src/main/java/com/sip/restcrud/remote/APIUtils.java
@@ -5,7 +5,7 @@ public class APIUtils {
     private APIUtils(){
     };
 
-    public static final String API_URL = "http://192.168.43.165:86/api/";
+    public static final String API_URL = "http://192.168.43.165:85/users/";
 
     public static UserService getUserService(){
         return RetrofitClient.getClient(API_URL).create(UserService.class);
